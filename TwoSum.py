d735cbc9040dcc6d16a3f026cfd02e3d4aac5992
Kevin <kliuengineering@gmail.com>
2024-09-24 01:16:16 -0400

    NeetCode Arrays & Hashing -> TwoSum

---
+++ TwoSum.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        output = []
+        KB = {}
+
+        for i in range(0, len(nums)):
+            x, y = nums[i], target - nums[i]
+            if y not in KB:
+                KB[ nums[i] ] = i
+            else:
+                output.append( i )
+                output.append( KB[y] )
+
+        output.sort()
+        return output
+    
+
+def main():
+    MyList = [5, 5]
+    target = 10
+    solution = Solution()
+    print(solution.twoSum(MyList, target))
+
+
+if __name__ == "__main__":
+    main()
